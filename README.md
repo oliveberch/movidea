2dc8a194b2c5192664f40351c7c36767a540cf22
arc1124 <architgupta737@gmail.com>
2023-01-29 02:12:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Movidea
+
+Movidea is a webApp using react hooks which will be able to search for movies via the OMDB API and render the results to the user. The reason for building the app is just so that I can get a better understanding of using hooks in an application.
---
+++ src/component/App.js
@@ -0,0 +1,98 @@
+import React, { useReducer, useEffect } from "react";
+import "./App.css";
+import Header from "./header.js";
+import Movie from "./movie.js";
+import Search from "./search.js";
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=2962c5a0";
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null,
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload,
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: jsonResponse.Search,
+        });
+      });
+  }, []);
+
+  const search = (searchValue) => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST",
+    });
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=2962c5a0`)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        if (jsonResponse.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search,
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error,
+          });
+        }
+      });
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  return (
+    <div className="App">
+      <Header text="MOVIDEA" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a collection of my favorite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
